82bc767a478d4d62278d03c0cd9716b305920922
kimbumsoo0820 <kbs2082@naver.com>
2023-10-31 23:00:48 +0900

    찜목록 디자인 작업

---
+++ src/components/favorit/FavoritCard.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import './FavoritCardCss.scss'
+
+
+export default function Home() {
+
+    return (
+        <div>
+           
+        </div>
+    );
+}
+
