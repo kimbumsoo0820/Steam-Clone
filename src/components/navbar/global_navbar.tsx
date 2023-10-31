1398432dec6130a966d47dd5c3ea7bd13806d034
Joseggi <lks004@naver.com>
2023-10-31 21:59:27 +0900

    ADD navbar

--- src/components/navbar/global_navbar.tsx
+++ src/components/navbar/global_navbar.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import {useState} from "react";
+import "./golbal_navbar.scss";
+
 
 
 function Navbar() {
+  const [isLogin, setLogin] = useState(true);
+
   return (
     <div className="navbar">
       <div className="content">
         <div className="left_area">
           <div className="logo">
-            left
+            <a href="#">
+              <div className="image"></div>
+            </a>
           </div>
           <div className="headerMenu">
+            <a href="#" className="headerMenu_shop headerMenu_active">상점</a>
+            <a href="#" className="headerMenu_community">커뮤니티</a>
+            {
+              isLogin ? 
+              <>
+                <a href="#" className="headerMenu_username">joseggi</a>
+                <a href="#" className="headerMenu_chatting">채팅</a>
+              </> :
+              <a href="#" className="headerMenu_infomation">정보</a>
+            }
+            <a href="" className="headerMenu_support">지원</a>
       
           </div>
         </div>
@@ -19,6 +37,17 @@ function Navbar() {
       </div>
     </div>
   )
+}
+
+function HederMenuLogin(props:Boolean) {
+  if (props) {
+    return(
+      <>
+        
+      </>
+      
+    )
+  }
 }
 
 export default Navbar;
