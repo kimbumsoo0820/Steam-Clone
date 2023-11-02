f69a22e5dfc47e04c16daebc1cd154d4dd41ec4b
Joseggi <lks004@naver.com>
2023-11-02 17:12:39 +0900

    ADD fullScreenbg

---
+++ src/components/homebackground/FullScreenBackground.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './FullScreenBackgroundCss.scss';
+
+export default function FullScreenBackground() {
+
+  return(
+    <div className='fullScreenBg'>
+      <div>
+        <video className='fullScreenVideo' poster="https://cdn.akamai.steamstatic.com/steam/clusters/frontpage/8ad799076d8330ab5503472c/page_bg_koreana.jpg?t=1698557339">
+						<source src="https://cdn.akamai.steamstatic.com/steam/clusters/frontpage/8ad799076d8330ab5503472c/webm_page_bg_koreana.webm?t=1698557339" type="video/webm"></source>
+						<source src="https://cdn.akamai.steamstatic.com/steam/clusters/frontpage/8ad799076d8330ab5503472c/mp4_page_bg_koreana.mp4?t=1698557339" type="video/mp4"></source>
+				</video>
+      </div>
+    </div>
+  )
+}
