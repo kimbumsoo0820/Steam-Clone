cae69725a40ac50d32e7dc1b0be3d5c52430e15d
kimbumsoo0820 <kbs2082@naver.com>
2023-11-02 23:03:08 +0900

    sort옵션 redux값 불러오기 및 저장

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./modules/game";
+import options from "./modules/favoritSort"
 
 const store = configureStore({
     reducer: {
-      game:counterSlice.reducer
+      // game:counterSlice.reducer,
+      favoritSort: options.reducer,
     },
   });
   
   export type RootState = ReturnType<typeof store.getState>;
   export type AppDispatch = typeof store.dispatch;
+
+
   export default store;
