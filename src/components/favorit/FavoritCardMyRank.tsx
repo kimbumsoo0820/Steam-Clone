b4439cbc9e4d512c41f1520e06c1dcc9fd8e629e
kimbumsoo0820 <kbs2082@naver.com>
2023-11-06 15:35:35 +0900

    찜 항목 카드

---
+++ src/components/favorit/FavoritCardMyRank.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import './FavoritCardCss.scss'
+import { ReactSortable } from "react-sortablejs-typescript";
+
+interface ItemType {
+    id: number;
+    name: string;
+  }
+
+
+
+export default function FavoritCardMyRank() {
+    const [tesetLIst, setTestList] = useState<ItemType[]>([
+        { id: 1, name: "shrek" },
+        { id: 2, name: "fiona" }
+    ]);
+    
+    return (
+        <>
+            <ReactSortable list={tesetLIst} setList={setTestList} handle=".handle">
+                {tesetLIst.map(item => (
+                    <div key={item.id}>{item.name}</div>
+                    
+                ))}
+            </ReactSortable>
+        </>
+    );
+}
+
