07cac55de681a737ab30ef7054a4c05a3e67293e
Joseggi <lks004@naver.com>
2023-11-26 03:11:33 +0900

    Footer 추가(Home, favorit)

--- src/page/Favorit/FavoritPage.tsx
+++ src/page/Favorit/FavoritPage.tsx
@@ -16,6 +16,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { RootState, AppDispatch } from '../../redux'
 import FavoritCardMyRank from '../../components/favorit/FavoritCardMyRank';
 
+import Footer from '../../components/footer/Footer';
+
 
 
 
@@ -32,96 +34,90 @@ import FavoritCardMyRank from '../../components/favorit/FavoritCardMyRank';
 
 
 export default function Home() {
-    let dispatch = useDispatch<AppDispatch>();
-    let [favoritCount, setFavoritCount] = useState<number>(5)
-    let [userName, setuserName] = useState<string>('삼겹살에 소주')
-    let [optionStatus, setOptionStatus] = useState<boolean>(false)
-    let [sortStatus, setsortStatus] = useState<boolean>(false)
-    let [loading, setLoading] = useState<boolean>(false)
-    let [gameData, setGameData] = useState<any>([])
-    
-    const gameList = useSelector((state: RootState) => state.favoritSort);
-    const firstLoad = useSelector((state: RootState) => state.favoritSort.firstLoad);
-
-    useEffect(() => {
-        if(firstLoad) {
-            dispatch(setGame(games))
-            dispatch(setFirst(false))
-        }
-        dispatch(setOptions(OptionSortSets))
-        dispatch(setSorts(SortSets))
-    }, [])
-
-    useEffect(()=> {
-        setLoading(true)
-        console.log('부모 gameData : ',gameList.games)
-        setGameData(gameList.games)
-    },[gameList.games])
-
-    function changeOptionStatus() {
-        setsortStatus(false)
-        setOptionStatus(!optionStatus)
+  let dispatch = useDispatch<AppDispatch>();
+  let [favoritCount, setFavoritCount] = useState<number>(5)
+  let [userName, setuserName] = useState<string>('삼겹살에 소주')
+  let [optionStatus, setOptionStatus] = useState<boolean>(false)
+  let [sortStatus, setsortStatus] = useState<boolean>(false)
+  let [loading, setLoading] = useState<boolean>(false)
+  let [gameData, setGameData] = useState<any>([])
+  
+  const gameList = useSelector((state: RootState) => state.favoritSort);
+  const firstLoad = useSelector((state: RootState) => state.favoritSort.firstLoad);
+  useEffect(() => {
+    if(firstLoad) {
+      dispatch(setGame(games))
+      dispatch(setFirst(false))
     }
-    function changeSortStatus() {
-        setOptionStatus(false)
-        setsortStatus(!sortStatus)
-    }
-
-    return (
-        
-        <div>
-            <div className=' outBox '>
-                <div className='navBarPadding'>
-                    <div className='contents'>
-                        <div className='favoritLine'>
-                            <div></div>
-                            <div className='favoritButton'>찜 목록 ({favoritCount})</div>
-                        </div>
-                        <div className='userProfile'>
-                            <div className='userPicture'>
-                                <img className='userPictureImage' src={`${process.env.PUBLIC_URL}/image/favoritPage/steamProfile.jpg`} alt="" />
-                            </div>
-                            <div className='userName'>{userName}님의 찜 목록</div>
-                        </div>
-                        <div className='searchSort'>
-                            <input type='text' className='searchInput' placeholder="이름 또는 태그로 검색"></input>
-                            <div className='optionSort' onClick={changeOptionStatus}>
-                                <div  >옵션</div>
-                                <div style={{ display: 'flex', alignItems: 'center' }}>
-                                    <img src="https://store.akamai.steamstatic.com/public/images/v6/btn_arrow_down_padded_white.png" alt="" />
-                                </div>
-                            </div>
-                            <div className='sortBox' onClick={changeSortStatus}>
-                                <div style={{ display: 'flex' }}>
-                                    <p style={{ color: '#1a9fff', marginRight: '5px' }}>정렬 기준:</p>내순위
-                                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                                        <img src="https://store.akamai.steamstatic.com/public/images/v6/btn_arrow_down_padded_white.png" alt="" />
-                                    </div>
-                                </div>
-
-                            </div>
-                            {optionStatus &&
-                                <div className='optionBox'>
-                                    <SortOptionBox />
-                                </div>
-                            }
-                            {sortStatus &&
-                                <div className='sortWrap'>
-                                    <SortSettingBox />
-                                </div>
-                            }
-                        </div>
-
-                        <div className='seperator'></div>
-                        {loading &&
-                            <>
-                                <FavoritCardMyRank myGame={gameData} />
-                            </>
-                        }
-                    </div>
-                </div>
+    dispatch(setOptions(OptionSortSets))
+    dispatch(setSorts(SortSets))
+  }, [])
+  useEffect(()=> {
+    setLoading(true)
+    console.log('부모 gameData : ',gameList.games)
+    setGameData(gameList.games)
+  },[gameList.games])
+  function changeOptionStatus() {
+    setsortStatus(false)
+    setOptionStatus(!optionStatus)
+  }
+  function changeSortStatus() {
+    setOptionStatus(false)
+    setsortStatus(!sortStatus)
+  }
+  return (
+    <div>
+      <div className=' outBox '>
+        <div className='navBarPadding'>
+          <div className='contents'>
+            <div className='favoritLine'>
+              <div></div>
+              <div className='favoritButton'>찜 목록 ({favoritCount})</div>
             </div>
+            <div className='userProfile'>
+              <div className='userPicture'>
+                <img className='userPictureImage' src={`${process.env.PUBLIC_URL}/image/favoritPage/steamProfile.jpg`} alt="" />
+              </div>
+              <div className='userName'>{userName}님의 찜 목록</div>
+            </div>
+            <div className='searchSort'>
+              <input type='text' className='searchInput' placeholder="이름 또는 태그로 검색"></input>
+              <div className='optionSort' onClick={changeOptionStatus}>
+                <div  >옵션</div>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <img src="https://store.akamai.steamstatic.com/public/images/v6/btn_arrow_down_padded_white.png" alt="" />
+                </div>
+              </div>
+              <div className='sortBox' onClick={changeSortStatus}>
+                <div style={{ display: 'flex' }}>
+                  <p style={{ color: '#1a9fff', marginRight: '5px' }}>정렬 기준:</p>내순위
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src="https://store.akamai.steamstatic.com/public/images/v6/btn_arrow_down_padded_white.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              {optionStatus &&
+                <div className='optionBox'>
+                    <SortOptionBox />
+                </div>
+              }
+              {sortStatus &&
+                <div className='sortWrap'>
+                  <SortSettingBox />
+                </div>
+              }
+            </div>
+            <div className='seperator'></div>
+            {loading &&
+              <>
+                <FavoritCardMyRank myGame={gameData} />
+              </>
+            }
+          </div>
         </div>
-    );
+      </div>
+			<Footer></Footer>
+    </div>
+  );
 }
 
